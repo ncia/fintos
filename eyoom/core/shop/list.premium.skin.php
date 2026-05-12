ab7d7eefd6af62786585b450c3204e4c26493af6
ncia <ncia@daum.net>
2026-05-12 17:39:49 +0900

    125번째 커밋: 메인 페이지 다중 상품 섹션 노출 오류 수정 - include_once를 include로 변경하여 모든 섹션 정상 출력

--- eyoom/core/shop/list.premium.skin.php
+++ eyoom/core/shop/list.premium.skin.php
@@ -17,13 +17,14 @@ $skin_dir = EYOOM_CORE_PATH . '/shop';
 /**
  * 상품리스트 공통 데이터 처리
  */
-include_once($skin_dir.'/list.skin.php');
+include($skin_dir.'/list.skin.php');
 
 /**
  * 테마 스킨 출력
  */
 if (file_exists(EYOOM_THEME_SHOP_SKIN_PATH.'/list.premium.skin.html.php')) {
-    include_once(EYOOM_THEME_SHOP_SKIN_PATH.'/list.premium.skin.html.php');
+    include(EYOOM_THEME_SHOP_SKIN_PATH.'/list.premium.skin.html.php');
 } else {
     echo "Skin file not found in theme path: " . EYOOM_THEME_SHOP_SKIN_PATH;
 }
+
